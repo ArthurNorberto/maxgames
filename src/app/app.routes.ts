9f33e9b02d27e86971faf114f5c0457dbfce201a
Arthur Norberto <arthurcostalima@hotmail.com>
2025-11-03 13:14:45 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    {
+        path: 'basket',
+        loadComponent: () =>
+            import('./pages/games/basquete/basquete.component').then(m => m.BasqueteComponent)
+    },
+    {
+        path: 'memory',
+        loadComponent: () =>
+            import('./pages/games/memory/memory.component').then(m => m.MemoryComponent)
+    },
+     {
+        path: 'milhao',
+        loadComponent: () =>
+            import('./pages/games/milhao/milhao.component').then(m => m.MilhaoComponent)
+    }
+    , { path: '**', redirectTo: '' }
+];
